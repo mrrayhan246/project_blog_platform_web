2db3d9c168afb18f7a3ac3e90327a8358eb590c3
MR R A Y H A N <mrrayhan246@gmail.com>
2025-02-02 21:38:12 +0600

    Update

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -6,3 +6,7 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'content')
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Enter your title'}),
+            'content': forms.Textarea(attrs={'class': 'form-textarea', 'placeholder': 'Write your content here...'}),
+        }
